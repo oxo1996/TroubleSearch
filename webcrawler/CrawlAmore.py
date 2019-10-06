6f87fb8552dece54d31c6084135ce8ffdf18046a
seong0318 <seong0318@naver.com>
2019-10-06 10:19:52 +0900

    다른 제품들까지 찾고 product list가 갱신 안되는 버그 해결

--- webcrawler/CrawlAmore.py
+++ webcrawler/CrawlAmore.py
@@ -4,12 +4,15 @@ from selenium import webdriver
 import re
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+import urllib.request
 import json
+import time
 
 class CrawlAmore(crawlable):
-    def __init__(self, path):
+    def __init__(self, path, ctg):
         self.mall_link = "https://www.amorepacificmall.com"
         self.mall_category = path
+        self.category = ctg
         self.driver = webdriver.Chrome("chromedriver")
 
     def _cleanText(self, readData):
@@ -28,13 +31,22 @@ class CrawlAmore(crawlable):
     def _getProductLink(self):
         html = self.driver.page_source
         soup = BeautifulSoup(html, "html.parser")
-        product_links = soup.select("div.product_box > a")
+        productLinks = soup.select("div.product_box > a")
 
         links = {}
-        for link in product_links:
+        for link in productLinks:
             name = self._cleanText(link.get("ap-click-data"))
             links[name] = self.mall_link + link.get("href")
         return links
+
+    def _getBrand(self):
+        brand = self.driver.find_element_by_xpath("//div[@class='product_brand']/a")
+        return brand.text
+
+    def _getImage(self, imgName):
+        imageLink = self.driver.find_element_by_xpath("//li[@class='ix-list-item']/img").get_attribute("src")
+        urllib.request.urlretrieve(imageLink, "./img/" + imgName + ".png")
+        return imageLink
 
     def _getIngredients(self):    
         ingredients = []     
@@ -49,9 +61,6 @@ class CrawlAmore(crawlable):
         for ig in ingrStr:
             ingredients.append(ig.strip())
 
-        # ingredients[pname] = self._removeHtmlTag(str(ingr[0]))
-        # print("ingr: ", ingredients[pname])
-
         closeBtn = self.driver.find_element_by_xpath("//*[@class='layer_btns']/a")
         closeBtn.send_keys('\n')
 
@@ -62,7 +71,7 @@ class CrawlAmore(crawlable):
         reviewBtn = self.driver.find_element_by_xpath("//*[@id='ap_container']/div/div[3]/div[1]/ul/li[2]")        
         reviewBtn.click()
 
-        for i in range(2, 0, -1):            
+        for i in range(3, 0, -1):            
             reviewStat = "//*[@data-stat=\"" + str(i) + "\"]"
             reviewStatBtn = self.driver.find_element_by_xpath(reviewStat)
             reviewStatBtn.send_keys('\n')
@@ -86,27 +95,38 @@ class CrawlAmore(crawlable):
         return reviews
 
     def _crawling(self):
-        self.driver.implicitly_wait(3)
+        self.driver.implicitly_wait(10)
         self.driver.get(self.mall_category)
-        items = {}
+
+        with open('C:\dev\django\djangoproject\webcrawler\items.json') as data_file:    
+            items = json.load(data_file)
+        #items = {}
 
         page = 1
         while True:
-            print("page: ", page)
+            print("running...", self.category, page)
+            time.sleep(5)
             links = self._getProductLink()
-            for pname in links.keys():
-                link = links[pname]
-                self.driver.get(link)
-
-                items["name"] = pname
-                items["ingredients"] = self._getIngredients()                
-                items["reviews"] = self._getReview()
+            for pname in links.keys():        
+                if pname in items.keys():
+                    print("exist item")
+                    continue 
                 print(pname)
+                link = links[pname]                
+                self.driver.get(link)
+                
+                items[pname]= {}
+                items[pname]["categories"] = self.category
+                items[pname]["brand"] = self._getBrand()
+                items[pname]["imageLink"] = self._getImage(items[pname]["brand"] + "_" + pname)
+                items[pname]["ingredients"] = self._getIngredients()                
+                items[pname]["reviews"] = self._getReview()                
             
             self.driver.get(self.mall_category)
             page += 1
             nextPageXpath = "//*[@data-page=\"" + str(page) + "\"]"
             try:
+                self.driver.implicitly_wait(10)
                 nextPageBtn = self.driver.find_element_by_xpath(nextPageXpath)
                 nextPageBtn.send_keys('\n')
             except NoSuchElementException:
@@ -114,6 +134,7 @@ class CrawlAmore(crawlable):
                 break
             with open('items.json', 'w', encoding='utf-8') as make_file:
                 json.dump(items, make_file, indent="\t")
+        return items
 
     def getData(self):
         ingr = self._crawling()
--- webcrawler/init.py
+++ webcrawler/init.py
@@ -3,9 +3,19 @@ from CrawlAmore import CrawlAmore
 from CrawlKo2Eng import CrawlKo2Eng
 
 if __name__ == '__main__':
-    all_category = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001?upperMenuId=CTG001&categoryType=category"
-    skin_category = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001002001?upperMenuId=CTG001002001&categoryType=category"
-    amore = CrawlAmore(skin_category)
-    data = webcrawler.crawlData(amore)
+    allCategory = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001?upperMenuId=CTG001&categoryType=category"
+    skinCategory = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001002001?upperMenuId=CTG001002001&categoryType=category"
+    lotionCategory = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001002002?upperMenuId=CTG001002002&categoryType=category"
+    essenceCategory = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001002003?upperMenuId=CTG001002003&categoryType=category"
+    creamCategory = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001002004?upperMenuId=CTG001002004&categoryType=category"
+    mistCategory = "https://www.amorepacificmall.com/kr/ko/display/category/CTG001002006?upperMenuId=CTG001002006&categoryType=category"
+
+    execList = [(skinCategory, "toner"), (lotionCategory, "lotion"), (essenceCategory, "essence"), (creamCategory, "cream"), (mistCategory, "mist")]
+    #execList = [(lotionCategory, "lotion"), (essenceCategory, "essence"), (creamCategory, "cream"), (mistCategory, "mist")]
+    for exc in execList:
+        amore = CrawlAmore(exc[0], exc[1])
+        data = webcrawler.crawlData(amore)
+
+    print("fully success")
     # Ko2Eng = CrawlKo2Eng()
     # data = webcrawler.crawlData(Ko2Eng)
