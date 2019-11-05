647c42f774daf29799ada846e4d6f68adee7e6fb
seong0318 <seong0318@naver.com>
2019-11-05 20:47:19 +0900

    제품 전체 유사도를 정규 분포 적용해 계산 완료.

--- translator/__init__.py
+++ translator/__init__.py
@@ -3,4 +3,9 @@ from papagonmt import papagonmt
 
 if __name__ == '__main__':
     nmt = papagonmt()
-    print(nmt.translate("안녕하세요. 방금 밥 먹었어요.", "ko", "en"))
+    translatedStr = nmt.translate(\
+    "제품 잘 받았습니다. 배송이 빠르네요. 포장도 꼼꼼하게 해 주셨어요. \
+    시향을 해보지 않고 바로 구매했는데 잘 산 것 같아요. \
+    제가 좋아하는 향이라 다행입니다. 양도 많아서 오랫동안 사용할 수 있을 것 같습니다. \
+    잘 쓸게요.", "ko", "en")
+    print(translatedStr)
