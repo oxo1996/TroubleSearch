489268ce98a0c7e5466a87ed8d0bcdd0d0a03eaf
oxo1996 <kimsm7337@gmail.com>
2019-11-21 17:07:31 +0900

    리뷰추가

--- information/views.py
+++ information/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from .models import *
 import json
 from dlmodel.embedding.w2v_tfidf import W2vTfidf
-
+from dlmodel.embedding.w2v_tfidf import *
+from dlmodel.embedding.bert_bm25 import *
 def load(self):
     with open("/webcrawler/ingredients_in_items") as data_file:
         item = json.load(data_file)
     return item
 
 def information(request):
-    
+    with open("webcrawler/items.json") as data_file:    
+        items = json.load(data_file)
+
     #gender = request.POST['gender']
     product = request.POST.getlist('product')
     symptom = request.POST['symptom']
@@ -20,9 +23,14 @@ def information(request):
     components = component.objects.all()
     components.delete()
 
-
     temp = Items.objects.all()
     item = Items.objects.none()
+
+    productCount = len(product)
+    productReviews = []
+
+    for productIdx in range(productCount):
+        productReviews.append((_mostReview(symptom,product[productIdx],items)))
 
     recommendItems = _recommendItems(symptom,imodel)
     for elem in result:
@@ -38,11 +46,8 @@ def information(request):
     components1 = components[:3]
     components2 = components[3:6]
     components3 = components[6:9]
-    
 
-
-
-    return render(request, 'information.html', {'item' : item ,'components1':components1,'components2':components2,'components3':components3,'recommendItems':recommendItems})
+    return render(request, 'information.html', {'item' : item ,'components1':components1,'components2':components2,'components3':components3,'recommendItems':recommendItems ,'productReviews':productReviews})
 
 def _recommendItems(symptoms, imodel:W2vTfidf):
     item = Items.objects.none()
@@ -52,4 +57,18 @@ def _recommendItems(symptoms, imodel:W2vTfidf):
         item|= temp.filter(name = elem[0])
 
     return item
+
+def _mostReview(symptom,product,items):
+    
+    reviewList = []
+    result = []
+    bert = BertBm25()
+    reviewdata = items[product]["reviews"]
+    for review in reviewdata:
+        reviewList.append(review[1])
+    
+    reviewIdx = bert.most_similar(symptom, reviewList, 3)
+    for idx in reviewIdx:
+        result.append(reviewList[idx[0]])
+    return result
 
